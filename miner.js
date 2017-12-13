cdf47a0ca07fa6d37d3f5deb9ff4ff97b01116df
Brian Seymour <imbrianseymour@gmail.com>
2017-12-13 02:24:17 -0600

    add missing difficulty

--- miner.js
+++ miner.js
@@ -1,24 +1,5 @@
 const crypto = require('crypto');
 
-const genesis_block = {
-    version: 1,
-    timestamp: 1513150089910,
-    prev: '0000000000000000000000000000000000000000000000000000000000000000',
-    difficulty: 4,
-    inputs: 3,
-    input_hash: '509f4ecb21dbb57c42df28efd0bab8b38e5604b189b2da0f17d0986d22cd1f1d',
-    input_data: [
-        '173.21.85.32@040241-07:00: welcome to mini chain :)',
-        '173.21.85.32@040312-07:00: this is a new block',
-        '173.21.85.32@040234-07:00: cool, eh?',
-    ],
-    block_hash: '0000015d21c17b1c57342a095050b7b6ed3415410e49a654a908f232fce5ddd1',
-    nonce: 17484,
-    header() {
-        return this.version + '' + this.timestamp + this.prev + this.inputs + this.input_hash;
-    }
-};
-
 var Block = function(block) {
     Object.keys(block).forEach(key => {
         this[key] = block[key];
@@ -30,22 +11,42 @@ Block.prototype.header = function() {
 };
 
 Block.prototype.test = function(nonce) {
-    return crypto
+    var hash = crypto
         .createHash('sha256')
         .update(this.header() + nonce)
-        .digest('hex')
-        .slice(0, this.difficulty) === new Array(this.difficulty+1).join('0')
+        .digest('hex');
+
+    if (hash.slice(0, this.difficulty) === new Array(this.difficulty+1).join('0')) {
+        this.nonce = nonce;
+        this.block_hash = hash;
+        return true;
+    }
+
+    return false;
 };
 
-var active_block = new Block(genesis_block);
-
-for (var i = 0; i <= 2147483648; ++i) {
-    if (active_block.test(i)) {
-        active_block.nonce = i;
-        active_block.block_hash = crypto.createHash('sha256').update(active_block.header() + i).digest('hex');
-        break;
+var genesis_block = new Block({
+    version: 1,
+    timestamp: 1513150089910,
+    prev: '0000000000000000000000000000000000000000000000000000000000000000',
+    difficulty: 5,
+    inputs: 3,
+    input_hash: '509f4ecb21dbb57c42df28efd0bab8b38e5604b189b2da0f17d0986d22cd1f1d',
+    input_data: [
+        '173.21.85.32@040241-07:00: welcome to mini chain :)',
+        '173.21.85.32@040312-07:00: this is a new block',
+        '173.21.85.32@040234-07:00: cool, eh?',
+    ],
+    block_hash: '00000e0da50f6c5fe86ab6d37bf3beee04b7b49ccfc0d0c47f732e1344f37597',
+    nonce: 338835,
+    header() {
+        return this.version + '' + this.timestamp + this.prev + this.difficulty + this.inputs + this.input_hash;
     }
-}
+});
 
-console.log(active_block);
-console.log(active_block.test(active_block.nonce));
+for (var i = 0; i <= 2147483648; ++i)
+    if (genesis_block.test(i))
+        break;
+
+console.log(genesis_block);
+console.log(genesis_block.test(genesis_block.nonce));
