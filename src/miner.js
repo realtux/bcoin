bcb79e793f3ca62b4d009a41299133c39c2f7bd3
Brian Seymour <imbrianseymour@gmail.com>
2017-12-27 15:04:54 -0600

    mikes blocks

--- src/miner.js
+++ src/miner.js
@@ -3,14 +3,14 @@ const request = require('request-promise');
 const Transaction = require('./transaction');
 const Block = require('./block');
 
-const MASTER_NODE = '104.197.55.138';
-//const MASTER_NODE = '192.168.86.10';
+//const MASTER_NODE = '104.197.55.138';
+const MASTER_NODE = '192.168.86.10';
 const NODE_PORT = 4343;
 
 request
     ({
         method: 'POST',
-        url: 'http://' + node + ':' + NODE_PORT + '/pending_tx',
+        url: 'http://' + MASTER_NODE + ':' + NODE_PORT + '/pending_tx',
         json: true
     })
     .then(json => {
@@ -21,7 +21,7 @@ request
         // start a new block
         var block = new Block();
         block.init_new();
-        block.add_mint('EtvmzPdyfZvXnr1QEtKgMN4hhaviZ54V7FynAUsm9o9Z');
+        block.add_mint('8TUZUuFuTisKv5oQ9jSLDDsGm9pCbyDrYEMcMr2Kia29');
         block.add_tx(txs);
 
         for (var i = 0; i <= 2147483648; ++i)
